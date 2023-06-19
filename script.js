9cbba17b26833cffd8ed7c43ce3c794833179ef7
Tiago Venturini <76845622+tiagoventurini@users.noreply.github.com>
2023-06-19 14:56:05 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+class Produto {
+  construtor() {
+    this.id = 0;
+    this.nomeProduto = "";
+    this.valor = 0;
+  }
+  adicionar() {
+    alert("Vamos adicionar um produto!");
+  }
+  excluir() {
+    alert("Item deletado! ");
+  }
+}
+
+var produto = new Produto();
